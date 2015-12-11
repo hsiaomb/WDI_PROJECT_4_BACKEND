598fa0bc471c80da4a5c5c929ae9e9541c4b8cba
hsiaomb <hsiaomb@usc.edu>
2015-12-11 15:21:25 +0000

    User model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  local: {
+    username: { type: String },
+    fullname: { type: String },
+    image: { type: String },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    favorites: [],
+    history: []
+  }
+});
+
+userSchema.statics.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
