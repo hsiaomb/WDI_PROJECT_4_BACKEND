8a0b57e4f85af86dee210617a23b062a8db84383
hsiaomb <hsiaomb@usc.edu>
2015-12-12 13:50:52 +0000

    Initial one-room no users chat setup with socket.io

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ var cookieParser   = require("cookie-parser");
 var methodOverride = require("method-override");
 var jwt            = require('jsonwebtoken');
 var expressJWT     = require('express-jwt');
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
 var fs = require('fs');
-
 var app            = express();
 
 var config         = require('./config/config');
@@ -57,30 +54,12 @@ app.use(function (err, req, res, next) {
 var routes = require('./config/routes');
 app.use("/api", routes);
 
-app.listen(3000);
+var server = app.listen(3000);
+var io = require('socket.io')(server);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket){
-  console.log('someone connected');
-});
-nsp.emit('hi', 'everyone!');
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    console.log(msg);
+    io.emit('message', msg);
   });
 });
